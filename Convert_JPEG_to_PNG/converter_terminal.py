4e84d89cc368fc240857b3415d1208076c7a533b
Ajith Jupalli <114250052+AjitHjupalli@users.noreply.github.com>
2025-03-29 12:30:50 +0530

    Update converter_terminal.java

--- Convert_JPEG_to_PNG/converter_terminal.py
+++ Convert_JPEG_to_PNG/converter_terminal.py
@@ -1,3 +1,21 @@
-from PIL import Image
-im1 = Image.open('input.jpeg')  # takes input image from present folder
-im1.save('output.png')          # output image is generated the folder
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class ImageConverter {
+    public static void main(String[] args) {
+        try {
+            // Open input image
+            File inputFile = new File("input.jpeg");
+            BufferedImage image = ImageIO.read(inputFile);
+            
+            // Save as output PNG
+            File outputFile = new File("output.png");
+            ImageIO.write(image, "png", outputFile);
+            
+            System.out.println("Conversion successful: output.png");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
